e65e88bc45b5f5716df9ee04ba3844d8daf7feb6
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2020-12-25 00:53:28 +0200

    Add unlockMe project

---
+++ Web/UnlockMe.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,14 @@
+﻿namespace UnlockMe.Web.Areas.Administration.Controllers
+{
+    using UnlockMe.Common;
+    using UnlockMe.Web.Controllers;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [Area("Administration")]
+    public class AdministrationController : BaseController
+    {
+    }
+}
